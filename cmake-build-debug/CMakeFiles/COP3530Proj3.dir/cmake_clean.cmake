f65e2f2f053d6e0d2d7c4ada2df0216f905fcadc
Jordan Senken <jsenken@yahoo.com>
2021-04-18 23:36:21 -0400

    initialcommit

---
+++ cmake-build-debug/CMakeFiles/COP3530Proj3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COP3530Proj3.dir/main.cpp.o"
+  "COP3530Proj3"
+  "COP3530Proj3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COP3530Proj3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
